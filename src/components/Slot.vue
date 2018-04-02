122e8f539f05ef05fd3fa051706eb866affd0716
Ilya Pashkov <ispashkov@gmail.com>
2018-04-02 11:07:33 +0400

    initial commit

---
+++ src/components/Slot.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+		<h1 :style="{color: color}">I'm slot component</h1>
+		<slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotComponent',
+  data() {
+    return {
+      color: this.color
+    };
+  }
+};
+</script>
